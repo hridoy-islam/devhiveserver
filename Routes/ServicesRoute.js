ded4eceac9e1eb70e6e11023e229daad09290570
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-21 01:47:42 +0600

    service protect updated

--- Routes/ServicesRoute.js
+++ Routes/ServicesRoute.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const { createService, getService, updateService, deleteService, getSingleService} = require('../Controllers/serviceController')
-const { protect } = require('../middleware/authMiddleWare')
-const router = express.Router()
+const express = require("express");
+const {
+  createService,
+  getService,
+  updateService,
+  deleteService,
+  getSingleService,
+} = require("../Controllers/serviceController");
 
+const router = express.Router();
 
 // define the home page route
 
-router.post('/', createService)
+router.post("/", createService);
 
-router.get('/', getService)
+router.get("/", getService);
 
-router.get('/:id', getSingleService)
+router.get("/:id", getSingleService);
 
-router.delete('/:id', protect, deleteService)
+router.delete("/:id", deleteService);
 
-router.patch('/:id', protect, updateService)
+router.patch("/:id", updateService);
 
-module.exports = router
+module.exports = router;
