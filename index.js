7bd0bd8cfbf26e2344ef4be3328498a9b807ad68
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-25 10:46:45 +0600

    socket.io configured

--- index.js
+++ index.js
@@ -35,10 +35,56 @@ app.use("/admin", AdminRoute);
 app.use("/", swaggerServe, swaggerSetup);
 app.use("/api-docs", swaggerServe, swaggerSetup);
 // new route for chat here
+// app.use("/chat", ChatRoute);
+// app.use("/message", MessageRoute);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Devhive is running: ${port}`);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    // console.log(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room " + room);
+  });
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+    if (!chat.users) {
+      return console.log("chat.users not defined");
+    }
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) {
+        return;
+      }
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+  socket.off("setup", (userData) => {
+    console.log("user Disconnected");
+    socket.leave(userData._id);
+  });
 });
