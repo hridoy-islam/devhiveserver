6041b1c6fac1b579b24dfc0658eed9f5c20d4171
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-25 16:42:41 +0600

    updated index

--- index.js
+++ index.js
@@ -44,50 +44,51 @@ app.use("/api-docs", swaggerServe, swaggerSetup);
 app.use(notFound);
 app.use(errorHandler);
 
-const server = app.listen(port, () => {
+// const server =
+app.listen(port, () => {
   console.log(`Devhive is running: ${port}`);
 });
 
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = require("socket.io")(server, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log("connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    // console.log(userData._id);
-    socket.emit("connected");
-  });
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("user joined room " + room);
-  });
-  socket.on("typing", (room) => {
-    socket.in(room).emit("typing");
-  });
-  socket.on("stop typing", (room) => {
-    socket.in(room).emit("stop typing");
-  });
+// io.on("connection", (socket) => {
+//   console.log("connected to socket.io");
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     // console.log(userData._id);
+//     socket.emit("connected");
+//   });
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     console.log("user joined room " + room);
+//   });
+//   socket.on("typing", (room) => {
+//     socket.in(room).emit("typing");
+//   });
+//   socket.on("stop typing", (room) => {
+//     socket.in(room).emit("stop typing");
+//   });
 
-  socket.on("new message", (newMessageReceived) => {
-    var chat = newMessageReceived.chat;
-    if (!chat.users) {
-      return console.log("chat.users not defined");
-    }
-    chat.users.forEach((user) => {
-      if (user._id === newMessageReceived.sender._id) {
-        return;
-      }
-      socket.in(user._id).emit("message received", newMessageReceived);
-    });
-  });
-  socket.off("setup", (userData) => {
-    console.log("user Disconnected");
-    socket.leave(userData._id);
-  });
-});
+//   socket.on("new message", (newMessageReceived) => {
+//     var chat = newMessageReceived.chat;
+//     if (!chat.users) {
+//       return console.log("chat.users not defined");
+//     }
+//     chat.users.forEach((user) => {
+//       if (user._id === newMessageReceived.sender._id) {
+//         return;
+//       }
+//       socket.in(user._id).emit("message received", newMessageReceived);
+//     });
+//   });
+//   socket.off("setup", (userData) => {
+//     console.log("user Disconnected");
+//     socket.leave(userData._id);
+//   });
+// });
