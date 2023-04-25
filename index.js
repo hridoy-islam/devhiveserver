bcad5bccc5446181cb06cf351bcee28edc017996
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-25 16:51:13 +0600

    chatroute turned off for debug

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const UserRoute = require("./Routes/UserRoute");
 const ServicesRoute = require("./Routes/ServicesRoute");
 const CategoryRoute = require("./Routes/CategoryRoute");
 const AdminRoute = require("./Routes/AdminRoute");
-const ChatRoute = require("./Routes/ChatRoute");
-const MessageRoute = require("./Routes/MessageRoute");
+// const ChatRoute = require("./Routes/ChatRoute");
+// const MessageRoute = require("./Routes/MessageRoute");
 const bodyParser = require("body-parser");
 const { swaggerServe, swaggerSetup } = require("./Swagger-code/specs.js");
-// const { notFound, errorHandler } = require("./middleware/errMiddleWare");
+const { notFound, errorHandler } = require("./middleware/errMiddleWare");
 
 // middleware
 app.use(cors());
@@ -29,66 +29,63 @@ async function main() {
     useUnifiedTopology: true,
   });
 }
-app.get("/", (req, res) => {
-  res.send("api is running");
-});
+
 app.use("/user", UserRoute);
 app.use("/service", ServicesRoute);
 app.use("/category", CategoryRoute);
 app.use("/admin", AdminRoute);
-app.use("/chat", ChatRoute);
-app.use("/message", MessageRoute);
-// app.use("/", swaggerServe, swaggerSetup);
+// app.use("/chat", ChatRoute);
+// app.use("/message", MessageRoute);
+app.use("/", swaggerServe, swaggerSetup);
 app.use("/api-docs", swaggerServe, swaggerSetup);
 // new route for chat here
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
-// const server =
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Devhive is running: ${port}`);
 });
 
-// const io = require("socket.io")(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log("connected to socket.io");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData._id);
-//     // console.log(userData._id);
-//     socket.emit("connected");
-//   });
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("user joined room " + room);
-//   });
-//   socket.on("typing", (room) => {
-//     socket.in(room).emit("typing");
-//   });
-//   socket.on("stop typing", (room) => {
-//     socket.in(room).emit("stop typing");
-//   });
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    // console.log(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room " + room);
+  });
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
 
-//   socket.on("new message", (newMessageReceived) => {
-//     var chat = newMessageReceived.chat;
-//     if (!chat.users) {
-//       return console.log("chat.users not defined");
-//     }
-//     chat.users.forEach((user) => {
-//       if (user._id === newMessageReceived.sender._id) {
-//         return;
-//       }
-//       socket.in(user._id).emit("message received", newMessageReceived);
-//     });
-//   });
-//   socket.off("setup", (userData) => {
-//     console.log("user Disconnected");
-//     socket.leave(userData._id);
-//   });
-// });
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+    if (!chat.users) {
+      return console.log("chat.users not defined");
+    }
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) {
+        return;
+      }
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+  socket.off("setup", (userData) => {
+    console.log("user Disconnected");
+    socket.leave(userData._id);
+  });
+});
